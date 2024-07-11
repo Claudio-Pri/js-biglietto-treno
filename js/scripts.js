48f15867f47f5d5cd7e957e05d14aafa488087fb
Claudio-Pri <priv.claudio@gmail.com>
2024-07-11 15:31:16 +0200

    dichiarazione variabili

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+// dichiarazione variabili
 
+const passengerAge = prompt("Qual'è la tua età?");
+//come faccio a dire che può accettare solo numeri?
+console.log('Età passeggero', passengerAge);
+
+const travelDistance = prompt('Quanti chilometri devi percorrere?');
+console.log('Distanza da percorrere', travelDistance, 'km');
+
+// prezzo al chilometro
+const pricePerKm = 0.21;
+
+//prezzo base
+let prezzoBase = travelDistance * pricePerKm;
+let prezzoFinale;
+let sconto;
 
 
