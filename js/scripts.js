1c32e8c7957a64f5ed4ab188a0efb0347847f08d
Claudio-Pri <priv.claudio@gmail.com>
2024-07-11 15:43:55 +0200

    calcolo prezzo base

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,25 @@ console.log('Distanza da percorrere', travelDistance, 'km');
 
 // prezzo al chilometro
 const pricePerKm = 0.21;
+console.log('pricePerKm', pricePerKm, '€');
 
 //prezzo base
 let prezzoBase = travelDistance * pricePerKm;
+console.log('prezzo base', prezzoBase, '€');
+
 let prezzoFinale;
 let sconto;
+
+
+
+
+
+//solo senza sconto
+prezzoFinale = prezzoBase;
+
+document.getElementById('prezzo-biglietto').innerHTML = '€' + prezzoFinale;
+
+
+
 
 
